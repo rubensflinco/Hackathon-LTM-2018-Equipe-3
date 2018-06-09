5867b7c34dd1077a719b80871c8680665ad66c84
Emerson Soares <on.emersonsoares@gmail.com>
2018-06-09 16:36:12 -0300

    Fix multiplus and smiles

--- src/matchers/multiplus.js
+++ src/matchers/multiplus.js
@@ -4,7 +4,7 @@ const multiplusMatcher = (offer) => {
     const options = {
         method: 'get',
         url: 'https://www.pontosmultiplus.com.br/troque/casasbahia/typeahead',
-        params:{
+        params: {
             Dy: 1,
             Nty: 1,
             Ntt: 'CHAPINHA | PRANCHA MONDIAL GOLDEN ROSE P20 220°C CERÂMICA E TOURMALINE - ROSA'
@@ -14,33 +14,33 @@ const multiplusMatcher = (offer) => {
     return axios(options)
         .then(response => response.data.data)
         .then(data => {
-            const obj = []
-            for(let i = 0; i < data.suggestions.length - 1 ; i++){
-                if(data.suggestions[i].label == "Casas Bahia"){
-                    obj.vendor = "casasBahia"
+            const obj = {}
+            let vendor
+            for (let i = 0; i < data.suggestions.length - 1; i++) {
+                if (data.suggestions[i].label == "Casas Bahia") {
+                    vendor = "casasBahia"
                 }
-                if(data.suggestions[i].label == "Extra"){
-                    obj.vendor = "extra"
+                if (data.suggestions[i].label == "Extra") {
+                    vendor = "extra"
                 }
-                if(data.suggestions[i].label == "Ponto Frio"){
-                    obj.vendor = "pontoFrio"
+                if (data.suggestions[i].label == "Ponto Frio") {
+                    vendor = "pontoFrio"
                 }
-                if(data.suggestions[i].label == "Fast Shop"){
-                    obj.vendor = "fastShop"
+                if (data.suggestions[i].label == "Fast Shop") {
+                    vendor = "fastShop"
                 }
             }
-            
+
             data.results.forEach(element => {
                 obj.program = 'multiplus'
+                obj.vendor = vendor
                 obj.name = element.name
                 obj.pointPrice = element.price.full.to
                 obj.pointPriceFrom = element.price.full.from[0]
             })
-            
+
             return obj
         })
 }
-
-console.log(multiplusMatcher().then(resp => console.log(resp)))
 
 module.exports = multiplusMatcher
