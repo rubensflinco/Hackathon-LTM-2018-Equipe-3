abc54561a395f4e5b7fdad1ddbf52317eceaeee9
matheus <mklemann@live.com>
2018-06-09 16:26:30 -0300

    .

--- src/matchers/multiplus.js
+++ src/matchers/multiplus.js
@@ -1,7 +1,46 @@
-const multiplusMatcher = offer => (Promise.resolve({
-  program: 'multiplus',
-  vendor: offer.vendor,
-  pointsPrice: 200
-}))
+const axios = require('axios')
+
+const multiplusMatcher = (offer) => {
+    const options = {
+        method: 'get',
+        url: 'https://www.pontosmultiplus.com.br/troque/casasbahia/typeahead',
+        params:{
+            Dy: 1,
+            Nty: 1,
+            Ntt: 'CHAPINHA | PRANCHA MONDIAL GOLDEN ROSE P20 220°C CERÂMICA E TOURMALINE - ROSA'
+        }
+    }
+
+    return axios(options)
+        .then(response => response.data.data)
+        .then(data => {
+            const obj = []
+            for(let i = 0; i < data.suggestions.length - 1 ; i++){
+                if(data.suggestions[i].label == "Casas Bahia"){
+                    obj.vendor = "casasBahia"
+                }
+                if(data.suggestions[i].label == "Extra"){
+                    obj.vendor = "extra"
+                }
+                if(data.suggestions[i].label == "Ponto Frio"){
+                    obj.vendor = "pontoFrio"
+                }
+                if(data.suggestions[i].label == "Fast Shop"){
+                    obj.vendor = "fastShop"
+                }
+            }
+            
+            data.results.forEach(element => {
+                obj.program = 'multiplus'
+                obj.name = element.name
+                obj.pointPrice = element.price.full.to
+                obj.pointPriceFrom = element.price.full.from[0]
+            })
+            
+            return obj
+        })
+}
+
+console.log(multiplusMatcher().then(resp => console.log(resp)))
 
 module.exports = multiplusMatcher
