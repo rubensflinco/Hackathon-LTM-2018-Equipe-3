d1e32764c352957e3529e749b7d9ca91405d16b6
Emerson Soares <on.emersonsoares@gmail.com>
2018-06-09 18:48:21 -0300

    Procssing offers xlsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const program = require('commander')
 const offers = require('./offers')
 const productMatcher = require('./productMatcher')
+const util = require('util')
 
 offers('./offers.xlsx')
-  .then(offers => Promise.all(offers.map(productMatcher)))
-  .then(console.log)
+  .then(offers => offers.map(offer => {
+    return Promise.all(productMatcher(offer))
+      .then(match => ({
+        offer: { ...offer },
+        matches: match
+      }))
+  }))
+  .then(futureMatches => Promise.all(futureMatches))
+  .then(results => console.log(util.inspect(results, false, null)))
   .catch(console.log)
